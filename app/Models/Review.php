3b6cabd1e7b97f3f3767fa530e38f286a4299adc
areeba <areebatariq961@gmail.com>
2025-11-17 23:20:19 -0800

    added  cast for review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,11 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $fillable = [
-    'user_id',
-    'product_id',
-    'rating',
-    'comment',
-    'status', // 'approved', 'pending'
-];
+        'user_id',
+        'product_id',
+        'rating',
+        'comment',
+        'status', // 'approved', 'pending'
+    ];
+    protected $casts = [
+        'user_id' => 'integer',
+        'product_id' => 'integer',
+
+        'rating' => 'integer',  // usually 1-5 stars
+        'comment' => 'string',
+
+        'status' => 'string',   // 'approved' | 'pending'
+    ];
 
 }
