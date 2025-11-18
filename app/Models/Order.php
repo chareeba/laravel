742078e9d2a3afb9ec6f649adbb7c0865a609f16
areeba <areebatariq961@gmail.com>
2025-11-17 23:08:01 -0800

    added cast

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,14 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     protected $fillable = [
-    'user_id',
-    'order_number',
-    'total_amount',
-    'discount',
-    'shipping_cost',
-    'payment_status', // 'pending', 'paid', 'failed'
-    'order_status',   // 'pending', 'processing', 'completed', 'cancelled'
-    'shipping_address_id',
-    'payment_method', // 'cash_on_delivery', 'stripe', 'paypal', etc.
-];
+        'user_id',
+        'order_number',
+        'total_amount',
+        'discount',
+        'shipping_cost',
+        'payment_status', // 'pending', 'paid', 'failed'
+        'order_status',   // 'pending', 'processing', 'completed', 'cancelled'
+        'shipping_address_id',
+        'payment_method', // 'cash_on_delivery', 'stripe', 'paypal', etc.
+    ];
+
+    protected $casts = [
+        'user_id' => 'integer',
+        'order_number' => 'string',
+
+        'total_amount' => 'decimal:2',
+        'discount' => 'decimal:2',
+        'shipping_cost' => 'decimal:2',
+
+        'payment_status' => 'string',   // Or enum if you're using Laravel 9+
+        'order_status' => 'string',   // Or enum
+
+        'shipping_address_id' => 'integer',
+
+        'payment_method' => 'string',
+    ];
 }
