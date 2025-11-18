e019404bc85aaee741705def6db8f6b99bb3d47d
areeba <areebatariq961@gmail.com>
2025-11-17 23:18:48 -0800

    added caste for productvariant

--- app/Models/ProductVariant.php
+++ app/Models/ProductVariant.php
@@ -7,10 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 class ProductVariant extends Model
 {
     protected $fillable = [
-    'product_id',
-    'attribute', // e.g., 'Size'
-    'value',     // e.g., 'Large'
-    'price_adjustment',
-    'stock',
-];
+        'product_id',
+        'attribute', // e.g., 'Size'
+        'value',     // e.g., 'Large'
+        'price_adjustment',
+        'stock',
+    ];
+    protected $casts = [
+        'product_id' => 'integer',
+
+        'attribute' => 'string',  // e.g., 'Size', 'Color'
+        'value' => 'string',  // e.g., 'Large', 'Red'
+
+        'price_adjustment' => 'decimal:2', // can be positive or negative
+        'stock' => 'integer',
+    ];
+
 }
