b43c562ab52ec45e5eb44bf5da3ae5f289b95c66
areeba <areebatariq961@gmail.com>
2025-11-17 23:17:05 -0800

    added cast for product

--- app/Models/product.php
+++ app/Models/product.php
@@ -7,17 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {
     protected $fillable = [
-    'category_id',
-    'sub_category_id',
-    'name',
-    'slug',
-    'sku',
-    'price',
-    'discount_price',
-    'stock',
-    'short_description',
-    'long_description',
-    'status', // 'active' / 'inactive'
-    'featured', // boolean
-];
+        'category_id',
+        'sub_category_id',
+        'name',
+        'slug',
+        'sku',
+        'price',
+        'discount_price',
+        'stock',
+        'short_description',
+        'long_description',
+        'status', // 'active' / 'inactive'
+        'featured', // boolean
+    ];
+    protected $casts = [
+        'category_id' => 'integer',
+        'sub_category_id' => 'integer',
+        'name' => 'string',
+        'slug' => 'string',
+        'sku' => 'string',
+        'price' => 'decimal:2',
+        'discount_price' => 'decimal:2',
+        'stock' => 'integer',
+        'short_description' => 'string',
+        'long_description' => 'string',
+        'status' => 'boolean',   // active / inactive
+        'featured' => 'boolean',   // true / false
+    ];
+
 }
