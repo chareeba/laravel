bc95107656582ec303ee6904423e5ff953604dbc
areeba <areebatariq961@gmail.com>
2025-11-17 23:22:09 -0800

    added cast for shippingaddress

--- app/Models/ShippingAddress.php
+++ app/Models/ShippingAddress.php
@@ -7,16 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 class ShippingAddress extends Model
 {
     protected $fillable = [
-    'user_id',
-    'full_name',
-    'phone',
-    'email',
-    'address',
-    'city',
-    'state',
-    'postal_code',
-    'country',
-    'is_default',
-];
+        'user_id',
+        'full_name',
+        'phone',
+        'email',
+        'address',
+        'city',
+        'state',
+        'postal_code',
+        'country',
+        'is_default',
+    ];
+    protected $casts = [
+        'user_id' => 'integer',
+
+        'full_name' => 'string',
+        'phone' => 'string',
+        'email' => 'string',
+        'address' => 'string',
+        'city' => 'string',
+        'state' => 'string',
+        'postal_code' => 'string',
+        'country' => 'string',
+
+        'is_default' => 'boolean',   // true/false for default address
+    ];
 
 }
